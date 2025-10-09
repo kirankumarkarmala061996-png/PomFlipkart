4c90889489ed0bca1531d0fcb050aa2235bd15c1
kirankumarkarmala1996 <108362315+kirankumarkarmala1996@users.noreply.github.com>
2025-10-09 12:57:06 +0530

    Added new pages

---
+++ src/main/java/com/qa/flipkart/Pages/Login_page.java
@@ -0,0 +1,20 @@
+package com.qa.flipkart.Pages;
+
+import org.openqa.selenium.By;
+
+public class Login_page {
+
+	
+	public Login_page() {
+		
+	}
+	
+	By email = By.xpath("//input[@class='_2IX_2- VJZDxU']");
+	By password = By.xpath("//input[@type='password']");
+	By loginButton = By.xpath("//button[@class='_2KpZ6l _2HKlqd _3AWRsL']");
+	By closeLoginPopup = By.xpath("//button[@class='_2KpZ6l _2doB4z']");
+	By loginErrorMessage = By.xpath("//span[@class='_2YULOR']");
+	By forgotPasswordLink = By.xpath("//a[@class='_1g3afI']");
+	By requestOtpLink = By.xpath("//a[@class='_1g3afI']");
+	By signUpLink = By.xpath("//a[@class='_1g3afI']");
+}
